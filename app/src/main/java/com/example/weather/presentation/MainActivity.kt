faa38a0433bb354204faaded08fa86f454ddc269
RodionKirillov <rodion.44454@gmail.com>
2024-09-23 15:51:03 +0300

    16-add FavouriteContent UI

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.example.weather.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.arkivanov.decompose.defaultComponentContext
 import com.example.weather.WeatherApp
-import com.example.weather.data.network.api.ApiFactory
-import com.example.weather.data.network.api.ApiService
+import com.example.weather.domain.usecase.ChangeFavoriteStateUseCase
+import com.example.weather.domain.usecase.SearchCityUseCase
 import com.example.weather.presentation.root.DefaultRootComponent
 import com.example.weather.presentation.root.RootContent
-import com.example.weather.presentation.ui.theme.WeatherTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/weather/presentation/favorite/FavoriteContent.kt
+++ app/src/main/java/com/example/weather/presentation/favorite/FavoriteContent.kt
@@ -1,8 +1,101 @@
 package com.example.weather.presentation.favorite
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.weather.presentation.ui.theme.CardGradients
+import com.example.weather.presentation.ui.theme.Gradient
 
 @Composable
 fun FavoriteContent(component: FavoriteComponent) {
 
+    val state by component.model.collectAsState()
+
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        itemsIndexed(
+            items = state.cityItems,
+            key = { index, item -> item.city.id }
+        ) { index, item ->
+            CityCard(
+                cityItem = item,
+                index = index
+            )
+        }
+    }
+}
+
+@Composable
+private fun CityCard(
+    cityItem: FavoriteStore.State.CityItem,
+    index: Int
+) {
+    val gradient = getGradientByIndex(index)
+    Card(
+        modifier = Modifier.fillMaxSize()
+            .shadow(
+                elevation = 16.dp,
+                spotColor = gradient.shadowColor,
+                shape = MaterialTheme.shapes.extraLarge
+            ),
+        colors = CardDefaults.cardColors(containerColor = Color.Blue),
+        shape = MaterialTheme.shapes.extraLarge
+    ) {
+        Box(
+            modifier = Modifier
+                .background(gradient.primaryGradient)
+                .fillMaxSize()
+                .sizeIn(minHeight = 196.dp)
+                .drawBehind {
+                    drawRoundRect(
+                        brush = gradient.secondaryGradient,
+                        size = size,
+                        topLeft = Offset(
+                            x = 0f,
+                            y = size.height / 1.5f
+                        )
+                    )
+                }
+                .padding(24.dp)
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.BottomStart),
+                text = cityItem.city.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.background
+            )
+        }
+    }
+}
+
+private fun getGradientByIndex(index: Int): Gradient {
+    val gradients = CardGradients.gradients
+    return gradients[index % gradients.size]
 }
